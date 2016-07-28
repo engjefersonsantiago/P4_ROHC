d85aea150655a503b209e728ced91750e4ed85bb
Extrageek <loccer16@hotmail.com>
2016-07-28 11:30:38 -0700

    Changed the topo.txt to desired topology and changed the file names to match the current directory

--- P4/tutorials/examples/rohc_network/run_demo.sh
+++ P4/tutorials/examples/rohc_network/run_demo.sh
@@ -24,10 +24,10 @@ SWITCH_PATH=$BMV2_PATH/targets/simple_switch/simple_switch
 
 CLI_PATH=$BMV2_PATH/tools/runtime_CLI.py
 
-$P4C_BM_SCRIPT p4src/source_routing.p4 --json source_routing.json
+$P4C_BM_SCRIPT p4src/rohc_network.p4 --json rohc_network.json
 # This gives libtool the opportunity to "warm-up"
 sudo $SWITCH_PATH >/dev/null 2>&1
 sudo PYTHONPATH=$PYTHONPATH:$BMV2_PATH/mininet/ python topo.py \
     --behavioral-exe $SWITCH_PATH \
-    --json source_routing.json \
+    --json rohc_network.json \
     --cli $CLI_PATH
